e027357f52dd7d28790dbfa0c6ee4919ffee783b
manpreetkaurbagga2 <48022230+manpreetkaurbagga2@users.noreply.github.com>
2019-03-07 18:02:12 +0000

    Full code with Menu using switchcase

--- SequenceAnalyser.py
+++ SequenceAnalyser.py
@@ -1,34 +1,37 @@
 from Bio.Seq import Seq
 from Bio import SeqIO
 from Bio import SeqUtils
+import primer3
 
 class SequenceAnalyser():
     def __init__(self,seq):
         self.seq=seq
-            
-    def orf(self):
-        
+        self.ORF_Lenght=0
+    
+    def orf (self):
         my_sequences=[] 
         for i in range(len(self.seq)): 
-            if self.seq.startswith("ATG",i): 
-                for j in range(i,len(self.seq),3): 
-                #defining a condition to stop the loop if finds either TGA, TAA or TAG
-                    if self.seq.endswith("TGA",i,j) or self.seq.endswith("TAA",i,j) or self.seq.endswith("TAG",i,j):
-                        orf=self.seq[i:j]
+            if self.seq.startswith("ATG",i):  
+                for j in range(i,len(self.seq),3):                 
+                    if self.seq.endswith("TGA",i,j) or self.seq.endswith("TAA",i,j) or self.seq.endswith("TAG",i,j): 
+                        orf=self.seq[i:j] 
                         my_sequences.append(orf)
                         break 
-        longest_sequence=0
+        longest_sequence=0 
         longest_sequence_orf=my_sequences[0]
         for s in my_sequences: 
-            if len(s)>longest_sequence: 
+            if len(s)>longest_sequence:  
                 longest_sequence=len(s)
                 longest_sequence_orf=s
+        self.ORF_Lenght=len(longest_sequence_orf)
         return longest_sequence_orf
     
+    def __getattribute__(self, ORF_Lenght):
+        return super().__getattribute__(ORF_Lenght)
+   
 class Primer(SequenceAnalyser):    
     def __init__(self,seq):
         SequenceAnalyser.__init__(self,seq)
-        self.seq=seq
         self.A=seq.count("A")
         self.T=seq.count("T")
         self.G=seq.count("G")
@@ -47,8 +50,8 @@ def buildPrimer(seq,length):
     reverse_Complement =clippedReverseSeq.reverse_complement()
     Sequences = [forward_Complement,reverse_Complement]
     OutputString = ['Forward Primer\n ','Reverse Primer\n']
-    for loop in range(2):
-        clippedSeq = Sequences[loop]
+    for l in range(2):
+        clippedSeq = Sequences[l]
         lengthRange=[18,19,20,21,22,23,24,25]
         count=0
         for i in lengthRange:
@@ -66,7 +69,7 @@ def buildPrimer(seq,length):
                     print ("Melting Temperature : {}".format(p.meltingtemp()),file=open("output.txt", "a"))
                     print ("GC content : {}".format(p.GCcontent()),file=open("output.txt", "a"))
                     count=count+1
-        print("Total {0} Found :{1}\n".format(OutputString[loop],count),file=open("output.txt", "a"))
+        print("Total {0} Found :{1}\n".format(OutputString[l],count),file=open("output.txt", "a"))
 
 class Rbs(SequenceAnalyser):    
     def __init__(self,seq):
@@ -75,7 +78,7 @@ class Rbs(SequenceAnalyser):
    
     def RbsPosition(self):
         rbs_pos=SeqUtils.nt_search(str(self.seq),self.rbs)
-        return "RBS position : {}".format(rbs_pos)
+        return rbs_pos
 
 class RestrictionSites(SequenceAnalyser):
     def __init__(self,seq):
@@ -87,31 +90,62 @@ class RestrictionSites(SequenceAnalyser):
         
     def Pos_EcoRI(self):
         EcoRI_pos=SeqUtils.nt_search(str(self.seq),self.EcoRI)
-        return "EcoRI position : {}".format(EcoRI_pos)
+        return EcoRI_pos
     
     def Pos_XbaI(self):
         XbaI_pos=SeqUtils.nt_search(str(self.seq),self.XbaI)
-        return "XbaI position : {}".format(XbaI_pos)
+        return XbaI_pos
 
     def Pos_SpeI(self):
         SpeI_pos=SeqUtils.nt_search(str(self.seq),self.SpeI)
-        return "SpeI position : {}".format(SpeI_pos)
+        return SpeI_pos
 
     def Pos_PstI(self):
         PstI_pos=SeqUtils.nt_search(str(self.seq),self.PstI)
-        return "PstI position : {}".format(PstI_pos) 
-    
-for s in SeqIO.parse("id170383510.seq","fasta"): 
-    sequence=s.seq
-    abc=SequenceAnalyser(sequence)
-    buildingprimer(sequence)
-    print(abc.orf())
-    
-    a=Rbs(sequence)
-    print (a.RbsPosition())
-    
-    b=RestrictionSites(sequence)
-    print (b.Pos_EcoRI())
-    print (b.Pos_XbaI())
-    print (b.Pos_SpeI())
-    print (b.Pos_PstI())
+        return PstI_pos    
+  
+def menu():
+    print("Menu :\n")
+    print("Enter 1 to Find ORF of the Given Sequence")
+    print("Enter 2 to Find RBS")
+    print("Enter 3 to Find Biobrick compatible Restriction Sites")
+    print("Enter 4 to Find Forward and Reverse Primers")
+    print("Enter q to Quit.")
+    choice = input("Enter your Choice ?")
+    return choice
+
+   
+Sequence = Seq("")
+for s in SeqIO.parse("examplefile.seq","fasta"): #parsing the sequence file
+    Sequence=s.seq
+
+choice = menu()
+while (choice != 'q'):
+    if choice == '1':
+        a=SequenceAnalyser(Sequence)
+        print(a.orf(),file=open("output.txt", "a"))
+        more = input("Do you want to get the lenght of the ORF generated ?(Y/N)")
+        if more =='y' or more =='Y':
+            print("The Lenght of Genereted ORF : {}".format(a.ORF_Lenght),file=open("output.txt", "a"))
+            choice = menu()
+        else:
+            choice =menu()
+    elif choice == '2':
+        a = Rbs(Sequence)
+        print("RBS position : {}\n".format(a.RbsPosition()),file=open("output.txt", "a"))
+        choice = menu()
+    elif choice == '3':
+        a=RestrictionSites(Sequence)
+        print("EcoRI position : {}\n".format(a.Pos_EcoRI()),file=open("output.txt", "a"))
+        print("XbaI position : {}\n".format(a.Pos_PstI()),file=open("output.txt", "a"))
+        print("SpeI position : {}\n".format(a.Pos_SpeI()),file=open("output.txt", "a"))
+        print("PstI position : {}\n".format(a.Pos_XbaI()),file=open("output.txt", "a"))
+        choice = menu ()
+    elif choice == '4':
+        lengthToQuery = input("What lenght you want to query from forward and reverse?")
+        print("Primers",file=open("output.txt", "a"))
+        buildPrimer(Sequence,int(lengthToQuery))
+        choice = menu()
+    else:
+        break
+      
